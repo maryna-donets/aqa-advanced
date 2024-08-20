0ec4585ef4eff9d649dc83d3f783ebac6cad78d9
MarynaDonets <kotlyarmv@gmail.com>
2024-08-20 19:20:28 +0300

    js advanced

---
+++ jsAdvanced/task1.js
@@ -0,0 +1,15 @@
+function handleNum (num, callbackEven, callbackOdd) {
+    if (num % 2 === 0) {
+        callbackEven();
+      } else {
+        callbackOdd();
+      }
+}
+  function handleEven() {
+    console.log('number is even');
+  }
+  function handleOdd() {
+    console.log('number is odd');
+  }
+  handleNum(4, handleEven, handleOdd); 
+  handleNum(7, handleEven, handleOdd);
---
+++ jsAdvanced/task2.js
@@ -0,0 +1,8 @@
+function numbNum (num){
+    console.log(num);
+    if (num>0) {
+        return numbNum (num-1);
+    }
+}
+
+numbNum(5);
